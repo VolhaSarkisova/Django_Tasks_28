6b60403284eebe575eeea09baee2ed3f3d0562b4
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-05 14:58:22 +0300

    -fix: changeonline_platform.urls

--- online_platform/urls.py
+++ online_platform/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('comment/', include('comments.urls')),
-    # path('users/', include('users.urls')),
+    path('comment/', include('comments.urls')),
+    path('users/', include('users.urls')),
     path('', include('core.urls')),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
